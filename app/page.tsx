2e77b951f499c22e7b29db6d756976451794f17c
Mouadh Lassoued <mlassoued14@gmail.com>
2025-11-17 07:31:57 +0100

    QrCode2

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image"
 export default function QrCode () { 
   return (
   <div className="flex items-center justify-center h-screen bg-[hsl(212,45%,89%)]">
-    <div className="flex-row justify-center w-50 bg-[hsl(0,0%,100%)] rounded-2xl shadow-md">
-      <Image className={"p-2 rounded-2xl"} src={"/images/image-qr-code.png"} alt={"qr code"} width={250} height={250}/>
+    <div className="flex-row justify-center h-100 w-70 bg-[hsl(0,0%,100%)] rounded-4xl shadow-md">
+      <Image className={"p-5 rounded-4xl"} src={"/images/image-qr-code.png"} alt={"qr code"} width={300} height={250}/>
       <div className="text-center text-md font-bold">Improve your front-end skills by building projects</div>
       <div className="text-center text-xs p-2 font-thin">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</div>
     </div>
